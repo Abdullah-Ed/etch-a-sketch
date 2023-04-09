1fe737f2f0e955e1276226f528b4f933b0761373
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-04-09 19:53:39 +0200

    Create makeGrid function to generate a 16x16 grid layout

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container")
+
+
+function makeGrid(){
+  for(let i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+}
+
+makeGrid()
