b75c897c21e20721432841814a30374692f25496
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-04-10 18:56:13 +0200

    fix the makeGrid() to maintain grid layout

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const container = document.querySelector(".container")
 
 
-function makeGrid(){
-  for(let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
+function makeGrid(numSquares) {
+  // Calculate the width of each square based on the number of squares
+  // that is given in each row and column
+  const squareWidth = 100 / numSquares;
+ 
+  // Create new squares and add them to the container
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${squareWidth}%`;
+    square.style.paddingBottom = `${squareWidth}%`;
     container.appendChild(square);
   }
 }
 
-makeGrid();
+makeGrid(5);
 
 const square = document.querySelectorAll(".square");
 function paintSquare(){
@@ -21,3 +28,4 @@ function paintSquare(){
 };
 
 paintSquare();
+
