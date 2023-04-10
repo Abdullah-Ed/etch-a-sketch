a65e51ab7314184441b7257856046d2542163ea7
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-04-10 20:40:46 +0200

    Add Function to ask user for the number of squares in the grid
    This commit adds the function askNumber(), which prompts the user to
    enter a number between 1 and 100 to determine the number of squares
    in the grid.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container")
 
 
 function makeGrid(numSquares) {
+    // Clear the container
+    container.textContent = "";
   // Calculate the width of each square based on the number of squares
   // that is given in each row and column
   const squareWidth = 100 / numSquares;
@@ -16,10 +18,10 @@ function makeGrid(numSquares) {
   }
 }
 
-makeGrid(5);
+makeGrid(16);
 
-const square = document.querySelectorAll(".square");
 function paintSquare(){
+  const square = document.querySelectorAll(".square");
   square.forEach(square => {
     square.addEventListener("mouseover", event => {
       event.target.style.backgroundColor = "black";
@@ -29,3 +31,16 @@ function paintSquare(){
 
 paintSquare();
 
+
+function askNumber(){
+  let squareNumber = prompt("Pick a number between 1 and 100.", 16);
+  if(squareNumber >= 1 && squareNumber <= 100){
+    makeGrid(squareNumber);
+    paintSquare();
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+}
+
+const resetBtn =  document.querySelector(".reset-button") 
+resetBtn.addEventListener("click",askNumber)
